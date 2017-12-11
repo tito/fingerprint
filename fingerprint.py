4098d7174344bd0a2e70248406fb5f75775d8fc2
Mathieu Virbel <txprog@gmail.com>
2017-12-11 15:54:13 +0100

    first commit

---
+++ fingerprint.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Fingerprint implementation for android
+======================================
+
+Usage::
+
+    from fingerprint import Fingerprint
+
+    def on_authentication_succeeded(result):
+        print("It worked ?", result)
+
+    fp = Fingerprint(on_authentication_succeeded=on_authentication_succeeded)
+    fp.authenticate()
+
+"""
+
+from jnius import autoclass, cast, PythonJavaClass, java_method
+from kivy.event import EventDispatcher
+
+
+FingerprintManager = autoclass("android.hardware.fingerprint.FingerprintManager")
+activity = autoclass("org.kivy.android.PythonActivity").mActivity
+
+
+class PyFingerprintCallback(PythonJavaClass):
+    __javainterfaces__ = ["org/fingerprint/callback/FingerprintCallbackInterface"]
+    __javacontext__ = "app"
+
+    def __init__(self, dispatcher):
+        self.dispatcher = dispatcher
+        super(PyFingerprintCallback, self).__init__()
+
+    @java_method("(ILjava/lang/CharSequence;)V")
+    def onAuthenticationError(self, errorCode, errString):
+        self.dispatcher.dispatch(
+            "on_authentication_error", errorCode, errString)
+
+    @java_method("()V")
+    def onAuthenticationFailed(self):
+        self.dispatcher.dispatch("on_authentication_failed")
+
+    @java_method("(ILjava/lang/CharSequence;)V")
+    def onAuthenticationHelp(self, helpCode, helpString):
+        self.dispatcher.dispatch(
+            "on_authentication_help", helpCode, helpString)
+
+    @java_method("(Landroid.hardware.fingerprint."
+                 "FingerprintManager$AuthenticationResult;)V")
+    def onAuthenticationSucceeded(self, result):
+        self.dispatcher.dispatch(
+            "on_authentication_succeeded", result)
+
+
+class Fingerprint(EventDispatcher):
+
+    _manager = None
+    __events__ = (
+        "on_authentication_error",
+        "on_authentication_failed",
+        "on_authentication_help",
+        "on_authentication_succeeded"
+    )
+
+    @property
+    def manager(self):
+        if self._manager is None:
+            self._manager = cast(
+                FingerprintManager,
+                activity.getSystemService(FingerprintManager))
+            self._callback = PyFingerprintCallback(self)
+        return self._manager
+
+    def authenticate(self):
+        self._authenticate = self.manager.authenticate(self._callback)
+
+    def on_authentication_error(self, error_code, error_str):
+        pass
+
+    def on_authentication_failed(self):
+        pass
+
+    def on_authentication_help(self, help_code, help_string):
+        pass
+
+    def on_authentication_succeeded(self, result):
+        pass
